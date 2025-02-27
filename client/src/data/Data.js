ebe77a0173588e3b0a50227e0420d631a8050ddc
Eliza <eliza@Elizas-MacBook-Air.local>
2025-02-27 12:14:57 -0800

    Updated data.js

--- client/src/data/Data.js
+++ client/src/data/Data.js
@@ -26,6 +26,7 @@ import products13 from "../assets/products13.png";
 import products14 from "../assets/products14.png";
 import products15 from "../assets/products15.png";
 import products16 from "../assets/products16.png";
+import products20 from "../assets/products20.jpg";
 
 export const sliderItems = [
   {
@@ -73,82 +74,82 @@ export const newArrivals = [
   {
     id: 1,
     img: products1,
-    title: "Autumn Casual Dress",
-    price: 1000,
+    title: "Chic Blazer",
+    price: 4000,
   },
   {
     id: 2,
-    img: products2,
-    title: "Boho Summer Dress",
-    price: 1500,
+    img: products20,
+    title: "Assymetrical Wide Leg Pants",
+    price: 3500,
   },
   {
     id: 3,
     img: products3,
-    title: "Comfy Basic Top",
-    price: 1200,
+    title: "Ruffle One Sided Top",
+    price: 1900,
   },
   {
     id: 4,
     img: products4,
-    title: "Spring Vibes Top",
-    price: 1300,
+    title: "Comfy Skirt",
+    price: 200,
   },
   {
     id: 5,
-    img: products10,
-    title: "Spring Floral Dress",
-    price: 1600,
+    img: products5,
+    title: "Blazer Skirt Set",
+    price: 6000,
   },
   {
     id: 6,
-    img: products9,
-    title: "Casual Jacket",
-    price: 2000,
+    img: products6,
+    title: "Bow Top",
+    price: 2200,
   },
   {
     id: 7,
-    img: products11,
-    title: "Chic Blazer",
-    price: 1800,
+    img: products7,
+    title: "Cute Upshoulder Top",
+    price: 2200,
   },
 ];
 
 export const bestsellers = [
   {
     id: 5,
-    img: products5,
-    title: "Suede Leather Jacket",
+    img: products8,
+    title: "Ballerina Top",
     price: 3500,
   },
   {
     id: 6,
-    img: products6,
-    title: "Puffer Jacket",
+    img: products9,
+    title: "Wide Leg Jeans",
     price: 4000,
   },
   {
     id: 7,
-    img: products7,
-    title: "Warm Wool Coat",
-    price: 5000,
+    img: products10,
+    title: " Lining Shirt ",
+    price: 3000,
   },
   {
     id: 8,
-    img: products8,
-    title: "Winter Puffer Coat",
-    price: 4500,
+    img: products11,
+    title: "Shrug ",
+    price: 3000,
   },
   {
     id: 9,
     img: products12,
-    title: "Lightweight Jacket",
-    price: 2500,
+    title: "Cropped Pink Blazer",
+    price: 4000,
   },
   {
     id: 10,
     img: products13,
-    title: "Urban Chic Puffer",
+    title: "Silk Ruched Skirt",
     price: 3000,
   },
 ];
